b78aea733881601e37ef7d7e24d497a050df132f
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-25 01:44:24 +0200

    app.js update

--- server/app.js
+++ server/app.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const app = express();
+
 const userRoutes = require("./routes/users");
 const challengeRoutes = require("./routes/challenges");
+const authRoutes = require("./routes/auth");
 
 require("./db");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 app.use(express.json());
 
 app.use("/users", userRoutes);
 app.use("/challenges", challengeRoutes);
+app.use("/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
