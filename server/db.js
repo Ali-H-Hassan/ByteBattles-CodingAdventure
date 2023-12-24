30d331a261ea8c14c863ef6972308ce2330c5198
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-24 14:28:41 +0200

    Bug Fix

--- server/db.js
+++ server/db.js
@@ -4,7 +4,6 @@ mongoose
   .connect("mongodb://localhost:27017/bytebattle", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
