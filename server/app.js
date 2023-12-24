6a99620ba73696348549a87be85b800e06bc368e
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-24 14:21:24 +0200

    Importing Routes into main file

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const userRoutes = require("./routes/users");
+const challengeRoutes = require("./routes/challenges");
 
 require("./db");
 
@@ -8,3 +10,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use("/challenges", challengeRoutes);
