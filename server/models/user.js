72e7ba3a8c18a8a09a4551de3ca8e00650c58023
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-24 21:26:56 +0200

    Auth init Update User Model

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -55,5 +56,16 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model("User", userSchema);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 8);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = User;
