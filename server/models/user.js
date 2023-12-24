338351f16cf8041ca65eec19332ee02feb607d17
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-24 04:23:58 +0200

    Defining User Model

---
+++ server/models/user.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    // Ensure this is hashed
+    type: String,
+    required: true,
+  },
+  roles: [
+    {
+      type: String,
+      enum: ["learner", "admin", "company"],
+      default: "learner",
+    },
+  ],
+  experiencePoints: {
+    type: Number,
+    default: 0,
+  },
+  rank: {
+    type: Number,
+    default: 1,
+  },
+  learningPath: {
+    type: String,
+    enum: ["Frontend", "Backend"],
+  },
+  courseProgress: [
+    {
+      courseId: mongoose.Schema.Types.ObjectId,
+      progress: Number,
+      lastAccessed: Date,
+    },
+  ],
+  completedChallenges: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Challenge",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  lastLogin: Date,
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
