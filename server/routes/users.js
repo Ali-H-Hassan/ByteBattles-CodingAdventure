ac36e690b9c0d25f699cf8d4b2b5673749e472ff
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-27 00:59:43 +0200

    Updating User Roote

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,93 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const auth = require("../middlewares/auth");
+const userController = require("../controllers/userController");
 
-const jwtSecret = process.env.JWT_SECRET;
-
-//Read all
-router.get("/", auth, async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-//Read by ID
-router.get("/:id", auth, getUser, (req, res) => {
-  res.json(res.user);
-});
-
-//Create
-router.post("/", async (req, res) => {
-  const hashedPassword = await bcrypt.hash(req.body.password, 8);
-  const user = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: hashedPassword,
-  });
-
-  try {
-    const newUser = await user.save();
-    const token = jwt.sign({ _id: newUser._id }, jwtSecret, {
-      expiresIn: "24h",
-    });
-    res.status(201).json({ newUser, token });
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-//Update
-router.put("/:id", auth, getUser, async (req, res) => {
-  if (req.body.username != null) {
-    res.user.username = req.body.username;
-  }
-  if (req.body.email != null) {
-    res.user.email = req.body.email;
-  }
-  if (req.body.password != null) {
-    res.user.password = await bcrypt.hash(req.body.password, 8);
-  }
-
-  try {
-    const updatedUser = await res.user.save();
-    res.json(updatedUser);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-// DELETE
-router.delete("/:id", auth, async (req, res) => {
-  try {
-    const deletedUser = await User.findByIdAndDelete(req.params.id);
-    if (!deletedUser) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    res.json({ message: "Deleted User" });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-async function getUser(req, res, next) {
-  let user;
-  try {
-    user = await User.findById(req.params.id);
-    if (user == null) {
-      return res.status(404).json({ message: "Cannot find user" });
-    }
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
-  }
-
-  res.user = user;
-  next();
-}
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getUserById, (req, res) =>
+  res.json(res.user)
+);
+router.put("/:id", userController.getUserById, userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
