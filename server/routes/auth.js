4f39dbfc4fc7a893c73a707cae2c0a9ab2e81f6c
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-27 01:05:19 +0200

    Updating Auth Route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,64 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const User = require("../models/user");
 const passport = require("../middlewares/googleStrategy");
+const userController = require("../controllers/userController");
 
-const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
+// Use controller functions for register and login
+router.post("/register", userController.register);
+router.post("/login", userController.login);
 
-// Register
-router.post("/register", async (req, res) => {
-  try {
-    let user = await User.findOne({ email: req.body.email });
-    if (user) {
-      return res.status(400).send("User already registered.");
-    }
-
-    user = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: await bcrypt.hash(req.body.password, 8),
-    });
-    await user.save();
-    const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: "24h" });
-    res.status(201).send({ user, token });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-// Login
-router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      return res.status(400).send("Invalid email or password.");
-    }
-
-    const isMatch = await bcrypt.compare(req.body.password, user.password);
-    if (!isMatch) {
-      return res.status(400).send("Invalid email or password.");
-    }
-
-    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "24h",
-    });
-
-    res.send({ user, token });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+// Google OAuth routes remain here
 router.get(
   "/google",
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
-
 router.get(
   "/oauth2callback",
   passport.authenticate("google", { failureRedirect: "/login" }),
-  function (req, res) {
+  (req, res) => {
     // Successful authentication, redirect home.
     res.redirect("/");
   }
