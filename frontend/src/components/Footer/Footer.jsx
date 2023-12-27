e27cf6d88e334979431b271b4dcdfebc6ad559bc
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-27 23:14:24 +0200

    Footer Style init

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,65 +1,85 @@
 import React from "react";
 import "./Footer.css";
-import logo from "../../assets/LogoTransparent.png"; // Update with the path to your logo image
+import logo from "../../assets/Logo.png";
+import Facebook from "../../assets/Facebook.png";
+import Instagram from "../../assets/Instagram.png";
+import Twitter from "../../assets/Twitter.png";
+import LinkedIn from "../../assets/LinkedIn.png";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <div className="footer-top">
-          <div className="brand-container">
-            <img src={logo} alt="Byte Battles logo" className="logo" />
-            <div className="brand-name">Byte Battles</div>
+      <div className="footer-section">
+        <div className="footer-logo-contact">
+          <div className="footer-logo1">
+            <img src={logo} alt="Byte Battles logo" className="footer-logo1" />
+            <h2 className="footer-brand-name">Byte Battles</h2>
           </div>
-          <div className="address-contact">
-            <div className="address">
-              <div className="title">Address:</div>
-              <div>Hadath, Beirut</div>
-            </div>
-            <div className="contact">
-              <div className="title">Contact:</div>
-              <div>
-                +961 78 988 705
-                <br />
-                info@ByteBattles.io
-              </div>
-            </div>
-          </div>
+
+          <p className="footer-address">
+            <strong>Address:</strong>
+            <br />
+            Haddath, Beirut
+          </p>
+          <p className="footer-contact">
+            <strong>Contact:</strong>
+            <br />
+            +961 78 988 705
+            <br />
+            info@ByteBattles.io
+          </p>
           <div className="social-icons">
             {/* Placeholders for social icons */}
-            <div className="icon facebook"></div>
-            <div className="icon twitter"></div>
-            <div className="icon instagram"></div>
-            <div className="icon linkedin"></div>
+            <div className="icon facebook">
+              <img
+                src={Facebook}
+                alt="Byte Battles logo"
+                className="footer-logo"
+              />
+            </div>
+            <div className="icon twitter">
+              <img
+                src={Instagram}
+                alt="Byte Battles logo"
+                className="footer-logo"
+              />
+            </div>
+            <div className="icon instagram">
+              <img
+                src={Twitter}
+                alt="Byte Battles logo"
+                className="footer-logo"
+              />
+            </div>
+            <div className="icon linkedin">
+              <img
+                src={LinkedIn}
+                alt="Byte Battles logo"
+                className="footer-logo"
+              />
+            </div>
           </div>
         </div>
         <div className="footer-links">
-          <div className="links-column">
-            <a href="#">About Us</a>
-            <a href="#">Privacy Policy</a>
-            <a href="#">Careers</a>
-            <a href="#">Contact Us</a>
-            <a href="#">Terms and conditions</a>
-          </div>
-          <div className="links-column">
-            <a href="#">Courses</a>
-            <a href="#">Tests</a>
-            <a href="#">Company wise</a>
-            <a href="#">How to begin?</a>
-            <a href="#">Test wise</a>
-          </div>
+          <a href="#">About Us</a>
+          <a href="#">Privacy Policy</a>
+          <a href="#">Careers</a>
+          <a href="#">Contact Us</a>
+          <a href="#">Terms and conditions</a>
         </div>
-        <div className="footer-bottom">
-          <div className="line"></div>
-          <div className="bottom-content">
-            <div className="copyright">
-              Copyright © 2024 BYTEBATTLES. All right reserved.
-            </div>
-            <div className="policy-links">
-              <a href="#">Privacy Policy</a>
-              <a href="#">Terms of Service</a>
-            </div>
-          </div>
+        <div className="footer-links">
+          <a href="#">Courses</a>
+          <a href="#">Tests</a>
+          <a href="#">Company wise</a>
+          <a href="#">How to begin?</a>
+          <a href="#">Test wise</a>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>Copyright © 2024 BYTEBATTLES. All right reserved.</p>
+        <div className="footer-legal-links">
+          <a href="#">Privacy Policy</a>
+          <a href="#">Terms of Service</a>
         </div>
       </div>
     </footer>
