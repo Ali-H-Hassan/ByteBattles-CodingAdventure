dfb41be6f9598c4e5e1dd6e6c1807b57c5757a1b
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-28 04:53:27 +0200

    AuthContext init

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [authState, setAuthState] = useState({
+    isAuthenticated: false,
+    user: null,
+    token: null,
+  });
+
+  // Add functions to handle login, logout, and register here
+
+  return (
+    <AuthContext.Provider value={{ authState, setAuthState }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
