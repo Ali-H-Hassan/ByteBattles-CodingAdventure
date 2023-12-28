8497d09d0bac3557ec154ef9f56d9e0a415f3a0f
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-28 02:05:07 +0200

    Login Style init

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,33 +1,57 @@
 import React from "react";
+import Header from "../../components/Header/Header";
 import "./loginPage.css";
+import Login from "../../assets/LoginImg.png";
+import FbCard from "../../assets/FacebookCard.png";
+import GithCard from "../../assets/GithubCard.png";
+import GgCard from "../../assets/GoogleCard.png";
 
-const Login = () => {
+const LoginPage = () => {
   return (
-    <div className="login-container">
-      {" "}
-      <h1>Login</h1>{" "}
-      <div className="form-container">
-        {" "}
-        <label htmlFor="email">Email:</label>{" "}
-        <input type="email" id="email" name="email" required />{" "}
-        <label htmlFor="password">Password:</label>{" "}
-        <input type="password" id="password" name="password" required />{" "}
-        <button type="submit">Login</button> <p>Forgot Password?</p>{" "}
-      </div>{" "}
-      <div className="or-container">
-        {" "}
-        <hr /> <p>OR</p> <hr />{" "}
-      </div>{" "}
-      <div className="signup-container">
-        {" "}
-        <button type="button">Sign Up</button>{" "}
-      </div>{" "}
-      <p>
-        This site is protected by reCAPTCHA and the Google Privacy Policy and
-        Terms of Service apply.
-      </p>{" "}
+    <div className="login-page">
+      <Header />
+      <div className="login-body">
+        <div className="login-image-section">
+          <img src={Login} alt="Byte Battles Login" />
+        </div>
+        <div className="login-form-section">
+          <div className="login-container">
+            <h2 className="login-title">Byte Battle</h2>
+            <input type="email" id="email" name="email" placeholder="Mail Id" />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+            />
+            <button type="submit" className="login-button">
+              Log In
+            </button>
+            <div className="login-options">
+              <a href="#" className="forgot-password">
+                Forget Password?
+              </a>
+              <a href="#" className="signup-link">
+                Sign Up
+              </a>
+            </div>
+            <div className="social-login">
+              <p>Or you can Signup with</p>
+              <div className="social-icons">
+                <img src={GgCard} className="social-icon google" />
+                <img src={GithCard} className="social-icon github" />
+                <img src={FbCard} className="social-icon facebook" />
+              </div>
+              <p>
+                This site is protected by reCAPTCHA and the Google Privacy
+                Policy and Terms of Service apply.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Login;
+export default LoginPage;
