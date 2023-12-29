67d66d9e6fdae9bfeed4b5d2ecfa12e5ad0618c0
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-30 00:09:20 +0200

    Section 1 Styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Login from "./pages/LoginPage/LoginPage";
-import Signup from "./pages/SignupPage/signupPage";
+import Login from "../src/pages/LoginPage/LoginPage";
+import Signup from "../src/pages/SignupPage/signupPage";
+import WelcomePage from "../src/pages/WelcomePage/WelcomePage";
 import GoogleAuthHandler from "./pages/GoogleAuthHandler";
 import "./App.css";
 import GoogleCallbackHandler from "./pages/GoogleCallbackHandler";
@@ -15,6 +14,8 @@ function App() {
       <Router>
         <div className="App">
           <Routes>
+            <Route path="/" element={<WelcomePage />} />{" "}
+            {/* Add WelcomePage as the root route */}
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
             <Route
--- frontend/src/pages/WelcomePage/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage/WelcomePage.jsx
@@ -1,22 +1,31 @@
 // WelcomePage.jsx
 import React from "react";
-import FeatureSection from "../components/FeatureSection/FeatureSection";
-import TestimonialSection from "../components/TestimonialSection/TestimonialSection";
-import QuestionsSection from "../components/QuestionsSection/QuestionsSection";
-import "./WelcomePage.css"; // Assuming you have corresponding CSS file for WelcomePage
-
-const WelcomePage = () => {
+import OnlineTestIllustration from "../../assets/illustration.png";
+import "./WelcomePage.css";
+const WelcomeSectionOne = () => {
   return (
-    <div className="welcome-container">
-      <section className="hero-section">
-        {/* Content for the Hero section like title, subtitle, and call-to-action button */}
-      </section>
-      <FeatureSection />
-      <TestimonialSection />
-      <QuestionsSection />
-      {/* Add more sections or components as needed based on your design */}
-    </div>
+    <section className="welcome-section-one">
+      <div className="content-container">
+        <div className="text-content">
+          <h1>
+            <span className="green-text">A</span>
+            <span className="white-text"> New Way </span>
+            <span className="green-text">To Learn</span>
+          </h1>
+          <p>
+            Embark on your coding adventure. Choose your path – Frontend,
+            Backend, Software Engineering, and more. Dive into tutorial levels,
+            master the basics, and gear up for epic coding challenges. The
+            journey to coding mastery starts here!
+          </p>
+          <button className="cta-button">Create Account</button>
+        </div>
+        <div className="image-content">
+          <img src={OnlineTestIllustration} alt="Online Test" />
+        </div>
+      </div>
+    </section>
   );
 };
 
-export default WelcomePage;
+export default WelcomeSectionOne;
