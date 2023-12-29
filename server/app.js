65d2b85fe4f4bbd90604e5ea6872c13c2dcc0b37
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-29 04:20:59 +0200

    Main Server File Update

--- server/app.js
+++ server/app.js
@@ -1,34 +1,37 @@
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
-const passport = require("./middlewares/googleStrategy");
+// const passport = require("./middlewares/googleStrategy");
 const userRoutes = require("./routes/users");
 const challengeRoutes = require("./routes/challenges");
 const authRoutes = require("./routes/auth");
 require("./db");
 const cors = require("cors");
-
+const authRouter = require("./routes/oauth");
+const requestRouter = require("./routes/request");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 // Session and Passport middleware should be initialized before defining any routes
-app.use(
-  session({
-    secret: process.env.JWT_SECRET, // Use the secret from your environment variables
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+// app.use(
+//   session({
+//     secret: process.env.JWT_SECRET, // Use the secret from your environment variables
+//     resave: false,
+//     saveUninitialized: true,
+//   })
+// );
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // Define routes after middleware
 app.use("/users", userRoutes);
 app.use("/challenges", challengeRoutes);
 app.use("/api/auth", authRoutes); // If you want to keep this endpoint
+app.use("/oauth", authRouter);
+app.use("/request", requestRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
