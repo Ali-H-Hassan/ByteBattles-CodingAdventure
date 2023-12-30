5d17e129868ec1bbd2a9e51b532a28ec2fc357a9
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-30 23:50:50 +0200

    Leaderboard Structure init

---
+++ frontend/src/components/LeaderboardItem/LeaderboardItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./LeaderboardItem.css";
+
+const LeaderboardItem = ({ name, score }) => {
+  return (
+    <div className="leaderboard-item">
+      <span className="name">{name}</span>
+      <span className="score">{score}</span>
+    </div>
+  );
+};
+
+export default LeaderboardItem;
