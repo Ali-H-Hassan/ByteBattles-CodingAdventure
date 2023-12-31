e5653920bd5f9ba636503b5b10e3bcf25091e281
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-31 04:16:59 +0200

    UpComing Asset Added

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -14,6 +14,7 @@ import TestCard from "../../assets/Test1.png";
 import GoldMedal from "../../assets/gold-medal.png";
 import SilverMedal from "../../assets/silver-medal.png";
 import BronzeMedal from "../../assets/bronze-medal.png";
+import CalendarIcon from "../../assets/CalendarIcon.png";
 
 function Dashboard() {
   return (
