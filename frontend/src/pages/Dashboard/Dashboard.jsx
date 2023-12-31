a8a1d50d498b8bc9dd1d0d0375d8dcf1e59f9b15
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-31 04:28:31 +0200

    Stats Structure init

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -203,9 +203,54 @@ function Dashboard() {
             </div>
           </section>
 
-          <section className="grid-section">
-            {" "}
-            {/* Content for other sections */}{" "}
+          <section className="statistics">
+            <div className="statistic-card">
+              <img
+                src={TestsIcon}
+                alt="Tests Written"
+                className="statistic-icon"
+              />
+              <div className="statistic-info">
+                <span className="statistic-number">32</span>
+                <span className="statistic-text">Tests Written</span>
+              </div>
+            </div>
+            <div className="statistic-card">
+              <img src={PassedIcon} alt="Passed" className="statistic-icon" />
+              <div className="statistic-info">
+                <span className="statistic-number">12</span>
+                <span className="statistic-text">Passed</span>
+              </div>
+            </div>
+            <div className="statistic-card">
+              <img src={FailedIcon} alt="Failed" className="statistic-icon" />
+              <div className="statistic-info">
+                <span className="statistic-number">19</span>
+                <span className="statistic-text">Failed</span>
+              </div>
+            </div>
+            <div className="statistic-card">
+              <img
+                src={WaitingIcon}
+                alt="Waiting for Result"
+                className="statistic-icon"
+              />
+              <div className="statistic-info">
+                <span className="statistic-number">1</span>
+                <span className="statistic-text">Waiting for Result</span>
+              </div>
+            </div>
+            <div className="statistic-card">
+              <img
+                src={AverageIcon}
+                alt="Overall Average"
+                className="statistic-icon"
+              />
+              <div className="statistic-info">
+                <span className="statistic-number">%80</span>
+                <span className="statistic-text">Overall Average</span>
+              </div>
+            </div>
           </section>
         </div>
       </div>
