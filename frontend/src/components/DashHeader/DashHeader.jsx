64daf9ce329e7ee09fcd48452c7dcf8e3cb5837e
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-01 15:01:05 +0200

    Dashboard Header Component init

--- frontend/src/components/DashHeader/DashHeader.jsx
+++ frontend/src/components/DashHeader/DashHeader.jsx
@@ -1,13 +1,24 @@
-// import React from "react";
-// import "./StatCard.css";
+// Header.jsx
+import React from "react";
+import SearchIcon from "../../assets/search-icon.png"; // Update the path as needed
+import UserAvatar from "../../assets/Profile (1).png"; // Update the path as needed
 
-// const StatCard = ({ label, value }) => {
-//   return (
-//     <div className="stat-card">
-//       <div className="label">{label}</div>
-//       <div className="value">{value}</div>
-//     </div>
-//   );
-// };
+function Header() {
+  return (
+    <header className="main-header">
+      <div className="welcome-message">
+        <h1>Welcome Ali!</h1>
+        <p>Here is your Profile Dashboard</p>
+      </div>
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <img src={SearchIcon} alt="Search" />
+      </div>
+      <div className="profile-icon">
+        <img src={UserAvatar} alt="User Profile" />
+      </div>
+    </header>
+  );
+}
 
-// export default StatCard;
+export default Header;
--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,3 @@
-// Sidebar.jsx
 import React from "react";
 import Logo from "../../assets/FullLogo.png";
 import DashboardIcon from "../../assets/Chart.png";
