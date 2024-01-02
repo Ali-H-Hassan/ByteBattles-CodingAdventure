85ee098c2d6264a759097032d19c4e5724a2c21d
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-03 00:57:53 +0200

    Incorporating Phaser

---
+++ frontend/src/pages/GamePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Game from "../components/Game/Game";
+
+const GamePage = () => {
+  return (
+    <div>
+      <h1>Learn HTML with Phaser</h1>
+      <Game />
+    </div>
+  );
+};
+
+export default GamePage;
