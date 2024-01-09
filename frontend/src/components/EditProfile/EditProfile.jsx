acc3479c68559e08e722ab3eaaac74635cbb404f
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-10 00:26:39 +0200

    Frontend <> Initiating Basic Edit Profile Structure

---
+++ frontend/src/components/EditProfile/EditProfile.jsx
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import "./EditProfile.css";
+
+function EditProfile() {
+  const [profile, setProfile] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    address: "",
+    contactNumber: "",
+    country: "",
+    city: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProfile((prevProfile) => ({
+      ...prevProfile,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission
+    console.log(profile);
+  };
+
+  return (
+    <div className="edit-profile-container">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={profile.firstName}
+            onChange={handleChange}
+          />
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={profile.lastName}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={profile.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            value={profile.address}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="contactNumber">Contact Number</label>
+          <input
+            type="tel"
+            id="contactNumber"
+            name="contactNumber"
+            value={profile.contactNumber}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="country">Country</label>
+          <select
+            id="country"
+            name="country"
+            value={profile.country}
+            onChange={handleChange}
+          >
+            {/* Add options here */}
+          </select>
+          <label htmlFor="city">City</label>
+          <select
+            id="city"
+            name="city"
+            value={profile.city}
+            onChange={handleChange}
+          >
+            {/* Add options here */}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={profile.password}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-buttons">
+          <button type="button" className="cancel-button">
+            Cancel
+          </button>
+          <button type="submit" className="save-button">
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default EditProfile;
