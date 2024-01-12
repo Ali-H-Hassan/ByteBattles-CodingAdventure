5b8f5370f02872fc96eb3cf8e7247d340dbfc5e9
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-12 17:05:16 +0200

    Backend <> Updating Profile Controller

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -1,14 +1,21 @@
 const User = require("../models/user");
+const fs = require("fs");
+const path = require("path");
 
 exports.updateProfile = async (req, res) => {
   try {
     const userId = req.user._id;
     const updatedData = req.body;
 
-    await User.findByIdAndUpdate(userId, updatedData);
+    if (req.file) {
+      updatedData.profilePictureUrl = req.file.path;
+    }
+
+    await User.findByIdAndUpdate(userId, updatedData, { new: true });
 
     res.status(200).send("Profile updated successfully");
   } catch (error) {
+    console.error(error);
     res.status(500).send("Server error");
   }
 };
