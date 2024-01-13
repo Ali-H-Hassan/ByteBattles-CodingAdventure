498c88e89e22fdd75654d3155efcadcc7afafaaa
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-13 20:31:19 +0200

    Backend <> Updating Challenge Route

--- server/routes/challenges.js
+++ server/routes/challenges.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const challengeController = require("../controllers/challengeController");
 
 router.get("/", challengeController.getAllChallenges);
-router.get("/:id", challengeController.getChallengeById, (req, res) =>
-  res.json(res.challenge)
-);
+
+router.get("/random", challengeController.getRandomChallenge);
+
+router.get("/:id", challengeController.getChallengeById);
+
 router.post("/", challengeController.createChallenge);
-router.patch(
-  "/:id",
-  challengeController.getChallengeById,
-  challengeController.updateChallenge
-);
+
+router.patch("/:id", challengeController.updateChallenge);
+
 router.delete("/:id", challengeController.deleteChallenge);
 
 module.exports = router;
