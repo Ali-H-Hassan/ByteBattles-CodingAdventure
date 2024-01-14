daea98c4868acbcc5af3b83404d0248c8e5cb7a1
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-14 04:31:45 +0200

    Frontend <> Updating Results Display Structure

--- frontend/src/components/ResultsDisplay/ResultsDisplay.jsx
+++ frontend/src/components/ResultsDisplay/ResultsDisplay.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./ResultsDisplay.css";
+
 const ResultsDisplay = ({ results }) => {
   return (
-    <div>
+    <div className="results-display-container">
       <h3>Results</h3>
-      <div>User Efficiency: {results.user.efficiency}%</div>
-      <div>User Run Time: {results.user.runTime}</div>
-      <div>AI Efficiency: {results.ai.efficiency}%</div>
-      <div>AI Run Time: {results.ai.runTime}</div>
-      {/* You can add more details or a detailed comparison here */}
+      <div className="result-user">
+        User Efficiency: {results.user.efficiency}%
+      </div>
+      <div className="result-user">User Run Time: {results.user.runTime}</div>
+      <div className="result-ai">AI Efficiency: {results.ai.efficiency}%</div>
+      <div className="result-ai">AI Run Time: {results.ai.runTime}</div>
     </div>
   );
 };
