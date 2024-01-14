475f31ad6a5a04fb4ca0505fbc3f3f37794eb352
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-14 02:09:09 +0200

    Frontend <> Initiating Coding Editor Structure

---
+++ frontend/src/components/CodingEditor/CodingEditor.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-github";
+
+const CodingEditor = ({ code, handleCodeChange }) => {
+  return (
+    <AceEditor
+      mode="javascript"
+      theme="github"
+      value={code}
+      onChange={handleCodeChange}
+      name="UNIQUE_ID_OF_DIV"
+      editorProps={{ $blockScrolling: true }}
+      setOptions={{
+        enableBasicAutocompletion: true,
+        enableLiveAutocompletion: true,
+        enableSnippets: true,
+      }}
+    />
+  );
+};
+
+export default CodingEditor;
