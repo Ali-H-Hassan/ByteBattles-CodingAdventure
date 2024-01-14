57056da2fb7859d96f16032aaeed11de821219e1
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-14 04:21:19 +0200

    Frontend <> Replacing Ace Editor To Monaco

--- frontend/src/components/CodingEditor/CodingEditor.jsx
+++ frontend/src/components/CodingEditor/CodingEditor.jsx
@@ -1,27 +1,33 @@
 import React from "react";
-import AceEditor from "react-ace";
-import ace from "ace-builds/src-noconflict/ace"; // Ensure that ace is imported
-import "ace-builds/src-noconflict/mode-javascript";
-import "ace-builds/src-noconflict/theme-github";
+import MonacoEditor from "react-monaco-editor";
 import "./CodingEditor.css";
 
-// Set the base path if needed
-ace.config.set("basePath", process.env.PUBLIC_URL + "/ace");
-
 const CodingEditor = ({ code, handleCodeChange }) => {
+  const editorDidMount = (editor) => {
+    editor.focus();
+  };
+
+  const onChange = (newValue, e) => {
+    handleCodeChange(newValue);
+  };
+
+  const options = {
+    selectOnLineNumbers: true,
+    roundedSelection: false,
+    readOnly: false,
+    cursorStyle: "line",
+    automaticLayout: true,
+  };
+
   return (
-    <AceEditor
-      mode="javascript"
-      theme="github"
+    <MonacoEditor
+      height="400"
+      language="javascript"
+      theme="vs-dark"
       value={code}
-      onChange={handleCodeChange}
-      name="UNIQUE_ID_OF_DIV"
-      editorProps={{ $blockScrolling: true }}
-      setOptions={{
-        enableBasicAutocompletion: true,
-        enableLiveAutocompletion: true,
-        enableSnippets: true,
-      }}
+      options={options}
+      onChange={onChange}
+      editorDidMount={editorDidMount}
     />
   );
 };
