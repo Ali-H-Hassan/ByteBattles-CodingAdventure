ec09df6feca5e1c5d5bad6e11bfccf5466db2ec0
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-14 05:36:38 +0200

    Frontend <> Populating Auth Actions File

---
+++ frontend/src/actions/authActions.js
@@ -0,0 +1,15 @@
+import { LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT } from "./actionTypes";
+
+export const loginSuccess = (userData) => ({
+  type: LOGIN_SUCCESS,
+  payload: userData,
+});
+
+export const loginFailure = (error) => ({
+  type: LOGIN_FAILURE,
+  payload: error,
+});
+
+export const logout = () => ({
+  type: LOGOUT,
+});
