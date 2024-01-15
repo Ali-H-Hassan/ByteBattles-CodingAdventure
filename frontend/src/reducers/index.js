8893b4cd9ed61e9b13412e56fae356b92508610f
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-15 23:00:46 +0200

    Frontend <> Updating Index

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import challengeReducer from "./challengeReducer"; // Import the challenge reducer
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  challenge: challengeReducer, // Add the challenge reducer here
 });
 
 export default rootReducer;
