b7fcb9788845024c711947c573b113760187745c
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-15 19:44:06 +0200

    Backend <> Updating Users Route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,22 +3,7 @@ const router = express.Router();
 const passport = require("../middlewares/googleStrategy");
 const userController = require("../controllers/userController");
 
-// Use controller functions for register and login
 router.post("/register", userController.register);
 router.post("/login", userController.login);
-
-// // Google OAuth routes remain here
-// router.get(
-//   "/google",
-//   passport.authenticate("google", { scope: ["profile", "email"] })
-// );
-// router.get(
-//   "/oauth2callback",
-//   passport.authenticate("google", { failureRedirect: "/login" }),
-//   (req, res) => {
-//     // Successful authentication, redirect home.
-//     res.redirect("/");
-//   }
-// );
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const { authenticate, authorize } = require("../middlewares/auth");
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUserById, (req, res) =>
-  res.json(res.user)
+router.get("/", authenticate, authorize("admin"), userController.getAllUsers);
+router.get("/:id", authenticate, userController.getUserById);
+router.put("/:id", authenticate, authorize("admin"), userController.updateUser);
+router.delete(
+  "/:id",
+  authenticate,
+  authorize("admin"),
+  userController.deleteUser
 );
-router.put("/:id", userController.getUserById, userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+
+router.post(
+  "/admin/create-company",
+  authenticate,
+  authorize("admin"),
+  userController.createCompanyUser
+);
+
+router.post(
+  "/company/create-challenge",
+  authenticate,
+  authorize("company"),
+  userController.createChallenge
+);
 
 module.exports = router;
