327cbdf59262ca1e0dc070b1d3b93b2ae03237f0
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-16 17:15:23 +0200

    Backend <> Updating Challenge Route

--- server/routes/challenges.js
+++ server/routes/challenges.js
@@ -1,17 +1,38 @@
 const express = require("express");
 const router = express.Router();
 const challengeController = require("../controllers/challengeController");
+const { authenticate, authorize } = require("../middlewares/auth");
 
-router.get("/", challengeController.getAllChallenges);
+router.get(
+  "/",
+  authenticate,
+  authorize("admin"),
+  challengeController.getAllChallenges
+);
 
-router.get("/random", challengeController.getRandomChallenge);
+router.get("/random", authenticate, challengeController.getRandomChallenge);
 
-router.get("/:id", challengeController.getChallengeById);
+router.get("/:id", authenticate, challengeController.getChallengeById);
 
-router.post("/", challengeController.createChallenge);
+router.post(
+  "/",
+  authenticate,
+  authorize("company"),
+  challengeController.createTest
+);
 
-router.patch("/:id", challengeController.updateChallenge);
+router.patch(
+  "/:id",
+  authenticate,
+  authorize("company"),
+  challengeController.updateChallenge
+);
 
-router.delete("/:id", challengeController.deleteChallenge);
+router.delete(
+  "/:id",
+  authenticate,
+  authorize("admin"),
+  challengeController.deleteChallenge
+);
 
 module.exports = router;
