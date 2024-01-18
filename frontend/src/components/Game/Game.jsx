eb461b5d20d91f83524087179281f1c0699d7ee8
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-18 23:51:06 +0200

    Frontend <> Adding Redux To Game Component

--- frontend/src/components/Game/Game.jsx
+++ frontend/src/components/Game/Game.jsx
@@ -1,10 +1,22 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Phaser from "phaser";
 import GameScene from "./GameScene";
 import "./Game.css";
+import { submitScore } from "../../actions/gameActions";
 
 const GameComponent = ({ courseId }) => {
   const gameRef = useRef(null);
+  const dispatch = useDispatch();
+  const userId = useSelector((state) => state.auth.user._id);
+  const onGameComplete = useCallback(
+    (finalScore) => {
+      if (userId) {
+        dispatch(submitScore(userId, finalScore));
+      }
+    },
+    [dispatch, userId]
+  );
 
   useEffect(() => {
     const config = {
@@ -22,8 +34,7 @@ const GameComponent = ({ courseId }) => {
         gameRef.current.destroy(true);
       }
     };
-  }, [courseId]);
-
+  }, [courseId, onGameComplete]);
   return <div id="phaser-container"></div>;
 };
 
