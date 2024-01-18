c92473097ac0ce92b734cf01cbea46215b43dc55
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-19 00:44:06 +0200

    Frontend <> Fixing Bug In Game Component

--- frontend/src/components/Game/Game.jsx
+++ frontend/src/components/Game/Game.jsx
@@ -2,13 +2,23 @@ import React, { useEffect, useRef, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Phaser from "phaser";
 import GameScene from "./GameScene";
-import "./Game.css";
 import { submitScore } from "../../actions/gameActions";
 
 const GameComponent = ({ courseId }) => {
   const gameRef = useRef(null);
   const dispatch = useDispatch();
-  const userId = useSelector((state) => state.auth.user._id);
+  const user = useSelector((state) => state.auth.user);
+
+  const onGameComplete = useCallback(
+    (finalScore) => {
+      if (user && user._id) {
+        dispatch(submitScore(user._id, finalScore));
+      } else {
+        console.error("User ID not found, cannot submit score");
+      }
+    },
+    [dispatch, user]
+  );
 
   useEffect(() => {
     const config = {
@@ -26,13 +36,7 @@ const GameComponent = ({ courseId }) => {
         gameRef.current.destroy(true);
       }
     };
-  }, [courseId, dispatch]);
-
-  const onGameComplete = (finalScore) => {
-    if (userId) {
-      dispatch(submitScore(userId, finalScore));
-    }
-  };
+  }, [courseId, onGameComplete]);
 
   return <div id="phaser-container"></div>;
 };
