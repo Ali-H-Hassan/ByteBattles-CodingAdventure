eefbcf424f0fa0911fee0ca177b2169c69fb905f
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-18 19:49:45 +0200

    Frontend <> Updating Game Page

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import Game from "../components/Game/Game";
 
 const GamePage = () => {
+  const location = useLocation();
+  const { courseId } = location.state || {};
+
   return (
     <div>
-      <h1>Learn HTML with Phaser</h1>
-      <Game />
+      <h1>Game for Course ID: {courseId}</h1>
+      <Game courseId={courseId} />
     </div>
   );
 };
