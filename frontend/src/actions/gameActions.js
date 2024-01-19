17303f2bfd14b64e99d6694999be52cbf57702d6
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-19 22:37:52 +0200

    Backend <> Updating Game Actions

--- frontend/src/actions/gameActions.js
+++ frontend/src/actions/gameActions.js
@@ -1,17 +1,14 @@
-import apiClient from "../services/apiConfig";
-import { SUBMIT_SCORE } from "./actionTypes";
+import axios from "axios";
+import { FETCH_COURSES } from "./actionTypes";
 
-export const submitScore = (userId, score) => async (dispatch) => {
+export const fetchCourses = () => async (dispatch) => {
   try {
-    const response = await apiClient.post("/game/submit-score", {
-      userId,
-      score,
-    });
+    const response = await axios.get("/api/games/courses");
     dispatch({
-      type: SUBMIT_SCORE,
-      payload: response.data.highScore,
+      type: FETCH_COURSES,
+      payload: response.data,
     });
   } catch (error) {
-    console.error("Error submitting score:", error);
+    console.error("Error fetching courses", error);
   }
 };
