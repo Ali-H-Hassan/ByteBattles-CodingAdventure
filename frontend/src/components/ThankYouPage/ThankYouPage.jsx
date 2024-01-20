7117300d4eb5f41c9dbf3e4af0f8069e193ef504
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-20 22:41:28 +0200

    Frontend <> Adding Return Button

--- frontend/src/components/ThankYouPage/ThankYouPage.jsx
+++ frontend/src/components/ThankYouPage/ThankYouPage.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import TestHeader from "../../components/TestHeader/TestHeader";
 import Thanks from "../../assets/illustration.png";
 import "./ThankYouPage.css";
 
 const ThankYouPage = () => {
+  const navigate = useNavigate();
+
+  const returnToDashboard = () => {
+    navigate("/dashboard");
+  };
+
   return (
     <div className="thank-you-page-container">
+      <TestHeader timeLeft="00:00:00" />
       <div className="thank-you-content">
         <h1>Thanks For Participating</h1>
         <p>Your Test Has Completed!</p>
         <img src={Thanks} alt="Completed" className="thank-you-image" />
         <p>You will be notified when your test results are released</p>
+        <button className="thank-you-return-button" onClick={returnToDashboard}>
+          Return Home
+        </button>
       </div>
     </div>
   );
