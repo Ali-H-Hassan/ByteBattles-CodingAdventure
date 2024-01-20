81f95061ec8484c697b457006a130572a0b6c859
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-20 22:52:04 +0200

    Frontend <> Updating Test Header

--- frontend/src/components/TestHeader/TestHeader.jsx
+++ frontend/src/components/TestHeader/TestHeader.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./TestHeader.css";
 import Amazon from "../../assets/amazon.png";
+import Timer from "../../assets/Time Circle 3.png";
 const TestHeader = ({ timeLeft }) => {
   return (
     <div className="test-header">
@@ -9,7 +10,9 @@ const TestHeader = ({ timeLeft }) => {
         <span>TCS Quiz Competition</span>
       </div>
       <div className="test-header-timer">
-        <div className="timer-icon">⏲️</div>
+        <div className="timer-icon">
+          <img src={Timer} alt="Logo" className="timer" />
+        </div>
         <span>{timeLeft}</span>
       </div>
     </div>
