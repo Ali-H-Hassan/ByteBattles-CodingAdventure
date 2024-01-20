b0c11a123e879e22277627aa26a95b9909fb181a
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-20 20:10:21 +0200

    Frontend <> Initialising Thank You Page Structure

--- frontend/src/components/ThankYouPage/ThankYouPage.jsx
+++ frontend/src/components/ThankYouPage/ThankYouPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TestHeader from "../../components/TestHeader/TestHeader";
-
+import Thanks from "../../assets/illustration.png";
 import "./ThankYouPage.css";
 
 const ThankYouPage = () => {
@@ -10,11 +10,7 @@ const ThankYouPage = () => {
       <div className="thank-you-content">
         <h1>Thanks For Participating</h1>
         <p>Your Test Has Completed!</p>
-        <img
-          src="/path-to-your-asset-folder/thank-you-image.png"
-          alt="Completed"
-          className="thank-you-image"
-        />
+        <img src={Thanks} alt="Completed" className="thank-you-image" />
         <p>You will be notified when your test results are released</p>
       </div>
     </div>
