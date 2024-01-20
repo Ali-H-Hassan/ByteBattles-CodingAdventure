8006134444f470a6d011bc783d6adbd45494a5a7
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-20 04:05:46 +0200

    Frontend <> Updating Game Page

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Game from "../components/Game/Game";
+import { useSelector } from "react-redux";
 
 const GamePage = () => {
   const location = useLocation();
   const { courseId } = location.state || {};
+  const [courseName, setCourseName] = useState("Unknown Course");
+  const courses = useSelector((state) => state.game.courses);
 
-  const getCourseName = (id) => {
-    const courseNames = {
-      1: "HTML Basics",
-      2: "CSS Fundamentals",
-      3: "NodeJs Basics",
-      4: "Python Fundamentals",
-    };
-    return courseNames[id] || "Unknown Course";
-  };
-
-  const courseName = getCourseName(courseId);
+  useEffect(() => {
+    if (courses.length > 0 && courseId) {
+      const course = courses.find((c) => c._id === courseId);
+      if (course) {
+        setCourseName(course.title);
+      }
+    }
+  }, [courses, courseId]);
 
   return (
     <div>
