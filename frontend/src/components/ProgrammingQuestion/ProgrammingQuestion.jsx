39cd2f19d07a3bee544c65aa5677f41ca1533801
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-20 19:41:21 +0200

    Frontend <> Adding Coding Editor

--- frontend/src/components/ProgrammingQuestion/ProgrammingQuestion.jsx
+++ frontend/src/components/ProgrammingQuestion/ProgrammingQuestion.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import CodingEditor from "../CodingEditor/CodingEditor";
 import "./ProgrammingQuestion.css";
 
-const ProgrammingQuestion = ({ problemStatement, starterCode }) => {
+const ProgrammingQuestion = ({
+  problemStatement,
+  starterCode,
+  handleCodeChange,
+}) => {
   return (
     <div className="test-programming-container">
       <div className="test-problem-statement">{problemStatement}</div>
-      <textarea
-        className="test-code-editor"
-        defaultValue={starterCode}
-      ></textarea>
+      <CodingEditor code={starterCode} handleCodeChange={handleCodeChange} />
       <button className="test-submit-button">Submit Test</button>
     </div>
   );
