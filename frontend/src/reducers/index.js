262652a4fba812dfea7eb3805a1f41c04be79b07
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-21 04:49:06 +0200

    Frontend <> Updating Root Reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import gameReducer from "./gameReducer";
 import testReducer from "./testReducer";
+import testDetailsReducer from "./testDetailsReducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   game: gameReducer,
   test: testReducer,
+  testDetails: testDetailsReducer,
 });
 
 export default rootReducer;
