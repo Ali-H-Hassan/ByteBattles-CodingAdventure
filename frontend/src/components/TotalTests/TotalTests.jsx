50111e7dbd48007a9c4535d977073257b42faf85
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-22 19:13:36 +0200

    Frontend <> Initiating Total Tests Structure

---
+++ frontend/src/components/TotalTests/TotalTests.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./TotalTests.css";
+
+function TotalTests() {
+  return (
+    <div className="total-tests">
+      <h2>Total Tests Created</h2>
+      <p className="tests-number">34</p>
+      <button className="create-test-button">Create New Test</button>
+    </div>
+  );
+}
+
+export default TotalTests;
