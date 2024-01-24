fa5fc6a127956654a34a7ef12566a706fed113a4
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-24 16:14:26 +0200

    Frontend <> Updating Store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
 import gameReducer from "./game/gameSlice";
+import testReducer from "./test/testSlice";
+import testDetailsReducer from "./testDetails/testDetailsSlice";
+
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     game: gameReducer,
+    test: testReducer,
+    testDetails: testDetailsReducer,
   },
 });
