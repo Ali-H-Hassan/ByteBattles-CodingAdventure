cf0dfb62981308f23b9b1f51d9fba173fbf7ec14
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-01-29 06:31:11 +0200

    Backend <> Testing All Test Cases

--- server/test/getCoursesTest.js
+++ server/test/getCoursesTest.js
@@ -1,6 +1,6 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const app = require("../app"); // Update the path to your app
+const app = require("../app");
 const expect = chai.expect;
 chai.use(chaiHttp);
 
--- server/test/getRandomChallengeTest.js
+++ server/test/getRandomChallengeTest.js
@@ -1,6 +1,6 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const app = require("../app"); // Update the path to your app
+const app = require("../app");
 const expect = chai.expect;
 chai.use(chaiHttp);
 
@@ -8,7 +8,7 @@ describe("Get Random Challenge Endpoint", () => {
   it("should retrieve a random challenge and return a 200 status code", (done) => {
     chai
       .request(app)
-      .get("/api/challenges/random") // Ensure this matches your route
+      .get("/api/challenges/random")
       .end((err, res) => {
         if (err) {
           console.error("Error fetching random challenge:", err);
@@ -19,7 +19,6 @@ describe("Get Random Challenge Endpoint", () => {
         expect(res.body).to.be.an("object");
         expect(res.body).to.have.property("title");
         expect(res.body).to.have.property("description");
-        // Add more assertions based on the challenge structure
         done();
       });
   });
