abed5fc869bab6cd8e1f4fd899fb6573081118b9
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2026-01-20 22:49:05 +0200

    Frontend <> Updating Responsive Styles & Dynamic Header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,28 +1,34 @@
 import React from "react";
 import "./Header.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+
 const Header = () => {
   const navigate = useNavigate();
-  const handleCreateAccountClick = () => {
-    navigate("/login");
+  const location = useLocation();
+
+  const isLoginPage = location.pathname === "/login";
+  const isSignupPage = location.pathname === "/signup";
+
+  const handleAuthButtonClick = () => {
+    if (isLoginPage) {
+      navigate("/signup");
+    } else {
+      navigate("/login");
+    }
   };
+
   const handleLogoClick = () => {
     navigate("/");
   };
+
   return (
     <header className="header">
       <h1 className="logo" onClick={handleLogoClick}>
         Byte Battle
       </h1>
       <nav className="navigation">
-        <a href="/practice" className="nav-link">
-          Practice
-        </a>
-        <a href="/explore" className="nav-link">
-          Explore
-        </a>
-        <button className="nav-link signup" onClick={handleCreateAccountClick}>
-          Log In
+        <button className="nav-link signup" onClick={handleAuthButtonClick}>
+          {isLoginPage ? "Sign Up" : "Log In"}
         </button>
       </nav>
     </header>
