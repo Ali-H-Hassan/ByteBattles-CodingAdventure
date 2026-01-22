f34ea72b014de3168511c9b24912dd97c1ab411f
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2026-01-22 18:01:19 +0200

    Frontend <> Updating design of TestCard component

--- frontend/src/components/TestCard/TestCard.jsx
+++ frontend/src/components/TestCard/TestCard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { fetchTestById } from "../../redux/testDetails/testDetailsActions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBuilding } from "@fortawesome/free-solid-svg-icons";
 import "./TestCard.css";
 import defaultLogo from "../../assets/DefaultLogo.jpeg";
 
@@ -14,13 +16,16 @@ const TestCard = ({ test }) => {
   }
 
   const {
-    _id: id,
+    id: testId,
+    _id: mongoId,
     logo = defaultLogo,
     title = "No Title",
-    status = "defaultStatus",
+    companyName,
   } = test;
+  const id = testId || mongoId; // Support both id (SQL) and _id (MongoDB)
 
-  const handleTestClick = () => {
+  const handleTakeTest = (e) => {
+    e.stopPropagation();
     if (id) {
       dispatch(fetchTestById(id));
       navigate(`/tests/${id}`);
@@ -28,11 +33,21 @@ const TestCard = ({ test }) => {
   };
 
   return (
-    <div className={`test-card-main ${status}`} onClick={handleTestClick}>
-      <div className="test-card-header"></div>
+    <div className="test-card-main">
       <div className="test-card-body">
         <img src={logo} alt={`${title} logo`} className="test-logo" />
-        <h3 className="test-title">{title}</h3>
+        <div className="test-card-content">
+          <h3 className="test-title">{title}</h3>
+          {companyName && (
+            <div className="test-company">
+              <FontAwesomeIcon icon={faBuilding} className="test-company-icon" />
+              <span className="test-company-name">{companyName}</span>
+            </div>
+          )}
+        </div>
+        <button className="test-take-button" onClick={handleTakeTest}>
+          Take Test
+        </button>
       </div>
     </div>
   );
