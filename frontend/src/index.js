6a6f77aefefe9569adb0f242677bf227a0532904
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2026-02-11 00:54:36 +0200

    <Frontend> Fix ResizeObserver loop error from Monaco Editor

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// Fix ResizeObserver loop error (benign error from Monaco Editor)
+// This patches ResizeObserver to debounce notifications and prevent the error
+const debounce = (fn, delay) => {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn(...args), delay);
+  };
+};
+
+const OriginalResizeObserver = window.ResizeObserver;
+window.ResizeObserver = class ResizeObserver extends OriginalResizeObserver {
+  constructor(callback) {
+    super(debounce(callback, 20));
+  }
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
